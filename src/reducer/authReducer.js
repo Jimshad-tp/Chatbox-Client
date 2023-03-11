a336af9b5f7f95d981731a6f37975eacb022a364
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-03-11 22:42:42 +0530

    reducer added

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -1,3 +1,10 @@
-const authReducer = (state = {authDate:null,loading:false,error:false,updateLoading:false},action) => {
-    
+const authReducer = (state = { authDate: null, loading: false, error: false, updateLoading: false }, action) => {
+switch(action.type){
+    case "AUTH_START":
+        return {...state,loading:true,error:false}
+        case "AUTH_SUCCESS":
+            return {...state,authDate}
+        default :
+        return state;
+}
 }
