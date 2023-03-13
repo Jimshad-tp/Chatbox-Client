16775416a1d87ef6177d7c171562f4a2a8a44e6f
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2023-03-13 13:21:24 +0530

    reducer added

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -3,8 +3,13 @@ switch(action.type){
     case "AUTH_START":
         return {...state,loading:true,error:false}
         case "AUTH_SUCCESS":
-            return {...state,authDate}
+            localStorage.setItem("profile",JSON.stringify({...action?.data}))
+            return {...state,authDate:action.data,loading:false,error:false}
+            case "AUTH_FAIL":
+                return {...state,loading:false,error:true}
         default :
         return state;
 }
 }
+
+export default authReducer;
